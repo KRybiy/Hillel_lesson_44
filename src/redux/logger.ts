dbed289557c9485329ea80b57129a2a03e76134e
Kate Rybiy <kporhun@gmail.com>
2024-07-31 11:23:31 -0700

    fixed logger type

--- src/redux/logger.ts
+++ src/redux/logger.ts
@@ -1,11 +1,11 @@
 import { Middleware, MiddlewareAPI, Dispatch, Action } from "@reduxjs/toolkit";
 import { RootState } from "./types";
 
-const logger: Middleware<object, RootState, Dispatch> = (storeApi: MiddlewareAPI<Dispatch<Action>, RootState>) => (next: Dispatch<Action>) => (action: Action) => {
+const logger: Middleware<Dispatch, RootState> = (storeApi: MiddlewareAPI<Dispatch<Action>, RootState>) => (next) => (action) => {
     const prevState = storeApi.getState().counter.count;
     const result = next(action);
     const nextState = storeApi.getState().counter.count;
-    console.log(`prev state: ${prevState}, \n\t Dispatch action: ${action.type}, \n\t next count: ${nextState}`);
+    console.log(`prev state: ${prevState}, \n\t Dispatch action: ${action}, \n\t next count: ${nextState}`);
     return result;
 };
 
